91287452625a2afd3403b8f657281fedfa4ceb2c
itstermoh <107225880+itstermoh@users.noreply.github.com>
2023-04-29 01:44:38 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -12,3 +12,20 @@ else
 print("Hello from script2.lua")
   end
 
+-- Get the key from the URL query string
+
+local key = game:GetService("HttpService"):UrlDecode(game:GetService("HttpService"):UrlEncode(game.Players.LocalPlayer.Name))
+
+-- Check if the key is valid
+
+if key == "hello" then
+
+  print("Valid key")
+
+  _G.keydone = true -- Set a global variable to indicate that the key has been validated
+
+else
+
+  print("Invalid key")
+
+end
