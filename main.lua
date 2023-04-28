f39467640bd3607976e7e990f5afab445f41af89
itstermoh <107225880+itstermoh@users.noreply.github.com>
2023-04-29 01:34:45 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 -- Define a function to load script2
 
-function load_script2()
 
-  -- Set a flag indicating that script2 was loaded through the key system
 
   _G.keydone = true
 
@@ -11,24 +9,4 @@ function load_script2()
   -- Load script2
 
   local script2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/itstermoh/test/main/script.lua"))()
-
-  if script2 then
-
-    -- Run script2
-
-    script2()
-
-  else
-
-    -- Handle the case where script2 could not be loaded
-
-    print("Error: script2.lua could not be loaded.")
-
-  end
-
-end
-
--- Call load_script2 to load script2
-
-load_script2()
 
